78092b4f1a24527ad637e38e675e17b441663d4b
SeongMin Park <ifndefdeadmau5@gmail.com>
2016-07-24 14:58:16 +0900

    Add source map and webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 module.exports = {
+    devtool: 'eval-source-map', // appropriate for experimental project, do not use for production build
     entry: __dirname + '/app/main.js',
     output: {
         path: __dirname + '/public',
         filename: 'bundle.js'
+    },
+
+    devServer: {
+        contentBase: "./public",
+        colors: true,
+        historyApiFallback: true,
+        inline: true
     }
 };
